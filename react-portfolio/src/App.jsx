26f80be630ca9f8368e8cbe9f8f053d4941f29bc
WaeBae <jrwuoft@gmail.com>
2024-06-10 15:35:59 -0400

    initial commit
    initialized react & vite
    
    Navbar.module
    created navbar module
    
    Navbar.module.css
    added basic responsiveness to navbar

---
+++ react-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+import styles from './App.module.css'
+import { Navbar } from '/src/components/Navbar/Navbar.jsx'
+
+function App() {
+
+  return (
+    <div className ={styles.App}>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
